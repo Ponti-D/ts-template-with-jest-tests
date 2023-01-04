ce67b811a3d332f6f63b22bfd600ba60aadd3439
Ponti-D <spaediaz@gmail.com>
2023-01-04 19:56:32 +0000

    clean up codes

--- src/simplePigLatin.ts
+++ src/simplePigLatin.ts
@@ -6,21 +6,16 @@ export function pigLatin(inputString: string): string {
   }
 
   let outPutString = inputString.split(" ");
-  return inputString.split(' ')
-  .map(x=> {
-    if(x.substring(1).match(/[aA-zZ]/)) {
-      x=`${x.substring(1)}${x.charAt(0)}ay`
-    }
-   return x;
-  })
-   .join(' ')
- 
-  // let result = outPutString.map((word) => {
-  //   if (word.substring(1).match(/[aA-zZ]/)) {
-  //     word = word.substring(1) + word.charAt(0) + "ay";
-  //   }
-  //   return word;
-  // });
- 
-  // return result.join(" ");
+
+  let result = inputString
+    .split(" ")
+    .map((x) => {
+      if (x.substring(1).match(/[aA-zZ]/)) {
+        x = `${x.substring(1)}${x.charAt(0)}ay`;
+      }
+      return x;
+    })
+    .join(" ");
+
+  return result;
 }
